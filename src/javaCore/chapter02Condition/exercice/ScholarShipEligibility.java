acd21a4c54abb3a670345a4878c72fdb4cc7e497
Alex.Bachir <alex.bachir@laplateforme.io>
2026-01-26 16:21:16 +0100

    add ScholarShipEligibility +

--- src/javaCore/chapter02Condition/exercice/ScholarShipEligibility.java
+++ src/javaCore/chapter02Condition/exercice/ScholarShipEligibility.java
@@ -1,4 +1,15 @@
 package javaCore.chapter02Condition.exercice;
 
 public class ScholarShipEligibility {
+    public static void main(String[] args) {
+
+
+        // ici on va tester supérieur à 3.5; egale à 3.5; inférieur à 3.5
+        double studentGpa = 2;
+        // ici on va tester le revenus du foyer si il est inférieur à 40.000 puis 
+        int householdIncome = 35000;
+        boolean hasExtracurricular = true;
+
+
+    }
 }
