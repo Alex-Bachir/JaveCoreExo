a8067cbebe2164bcfe9c9f13f157aa4b033b760a
Alex.Bachir <alex.bachir@laplateforme.io>
2026-01-30 10:11:46 +0100

    ScholarShipEligibility add condition

--- src/javaCore/chapter02Condition/exercice/ScholarShipEligibility.java
+++ src/javaCore/chapter02Condition/exercice/ScholarShipEligibility.java
@@ -5,13 +5,44 @@ public class ScholarShipEligibility {
 
 
         // ici on va tester supérieur à 3.5; egale à 3.5; inférieur à 3.5
-        double studentGpa = 2;
+        double studentGpa = 4;
         // ici on va tester le revenus du foyer si il est inférieur à 40.000 puis tester entre 40k et 60k puis au dessus de 60k
-        int householdIncome = 35000;
+        int householdIncome = 80000;
         // ici on va tester si il pratique une activé ou pas
         boolean hasExtracurricular = true;
 
-        
+//      Bourse complète :
+//      GPA > 3.5 ET revenu < 40000 ET activité extracurriculaire = OUI
+
+//      Bourse partielle :
+//      GPA > 3.5 ET revenu entre 40000-60000 ET activité extracurriculaire = OUI
+
+//      Pas de bourse (6 cas) :
+//
+//      GPA > 3.5 ET revenu < 40000 ET activité extracurriculaire = NON
+//      GPA > 3.5 ET revenu entre 40000-60000 ET activité extracurriculaire = NON
+//      GPA > 3.5 ET revenu > 60000 ET activité extracurriculaire = OUI
+//      GPA > 3.5 ET revenu > 60000 ET activité extracurriculaire = NON
+//      GPA ≤ 3.5 ET revenu < 40000 ET activité extracurriculaire = OUI
+//      GPA ≤ 3.5 ET revenu < 40000 ET activité extracurriculaire = NON
+//      GPA ≤ 3.5 ET revenu entre 40000-60000 ET activité extracurriculaire = OUI
+//      GPA ≤ 3.5 ET revenu entre 40000-60000 ET activité extracurriculaire = NON
+//      GPA ≤ 3.5 ET revenu > 60000 ET activité extracurriculaire = OUI
+//      GPA ≤ 3.5 ET revenu > 60000 ET activité extracurriculaire = NON
+
+
+
+
+        if (studentGpa > 3.5 && householdIncome < 40000 && hasExtracurricular) {
+            System.out.println("L'etudiant est éligible à la bourse.");
+        } else if (studentGpa > 3.5 && householdIncome < 60000 && hasExtracurricular) {
+            System.out.println("L'etudiant est éligible à la bourse partielle.");
+        } else if (studentGpa <= 3.5 && householdIncome >= 60000 && hasExtracurricular) {
+            System.out.println("L'etudiant n'est pas éligible à la bourse.");
+        } else {
+            System.out.println("L'etudiant n'est pas éligible à la bourse.");
+        }
+
 
 
     }
