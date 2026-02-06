ae2362c54cb524ee3aecd30dcdd4d45c1003224a
Alex.Bachir <alex.bachir@laplateforme.io>
2026-02-06 13:13:08 +0100

    add CoinChangerV2

--- src/javaCore/chapter02Condition/exercice/AgeValidation.java
+++ src/javaCore/chapter02Condition/exercice/AgeValidation.java
@@ -3,6 +3,16 @@ package javaCore.chapter02Condition.exercice;
 public class AgeValidation {
     public static void main(String[] args) {
 
+        final double MINIMUM_LEGAL_AGE = 18;
+        final double HOURS_IN_DAY      = 24;
+        final double DAYS_IN_MONTH     = 30;
+        final double MONTHS_IN_YEAR    = 12;
+        final double SECONDS_IN_MINUTE = 60;
+        final double SECONDS_IN_HOUR   = SECONDS_IN_MINUTE * SECONDS_IN_MINUTE; // 3600 secondes
+        final double SECONDS_IN_DAY    = HOURS_IN_DAY * SECONDS_IN_HOUR;        // 86 400 secondes
+        final double SECONDS_IN_MONTH  = DAYS_IN_MONTH * SECONDS_IN_DAY;        // 2 592 000 secondes
+        final double SECONDS_IN_YEAR   = MONTHS_IN_YEAR * SECONDS_IN_MONTH;     // 31 536 000 secondes
+
         /**
          * Début d'un code que nous ne comprenons pas à ce stade de la formation...
          */
@@ -24,23 +34,13 @@ public class AgeValidation {
         // On va tester un utilisateur majeur, puis un utilisateur pas majeur et un utilisateur présque majeur
         // A cette date 02.02.2026, les personnes nées en 2008 sont majeurs en 2026.
 
-        final double MINIMUM_LEGAL_AGE = 18;
-        final double HOURS_IN_DAY      = 24;
-        final double DAYS_IN_MONTH     = 30;
-        final double MONTHS_IN_YEAR    = 12;
-        final double SECONDS_IN_MINUTE = 60;
-        final double SECONDS_IN_HOUR   = SECONDS_IN_MINUTE * SECONDS_IN_MINUTE; // 3600 secondes
-        final double SECONDS_IN_DAY    = HOURS_IN_DAY * SECONDS_IN_HOUR;        // 86 400 secondes
-        final double SECONDS_IN_MONTH  = DAYS_IN_MONTH * SECONDS_IN_DAY;        // 2 592 000 secondes
-        final double SECONDS_IN_YEAR   = MONTHS_IN_YEAR * SECONDS_IN_MONTH;     // 31 536 000 secondes
-
         int userDayBirth   =   15; // jour de naissance
         int userMonthBirth =    2; // mois de naissance
         int userYearBirth  = 2020; // année de naissance
 
-        double userSinceBirth = currentYear - userYearBirth;
+        // plus court chemin pour connaitre approximativement la personne est majeur.
 
-        // plus court chemin pour connaitre approximativement la date de naissance d'une personne.
+        double userSinceBirth = currentYear - userYearBirth;
 
         if (userSinceBirth > MINIMUM_LEGAL_AGE) {
             System.out.println("Vous êtes majeur.");
@@ -50,11 +50,7 @@ public class AgeValidation {
         System.out.println("Vous êtes né le " + userDayBirth + "/" + userMonthBirth + "/" + userYearBirth + ". Vous avez donc " +  userSinceBirth + " ans.");
 
 
-
-
-
-
-
+        // ici on va essayer de connaître si il est majeur au jour près.
 
 
 
---
+++ src/javaCore/chapter02Condition/exercice/CoinChangerV2.java
@@ -0,0 +1,71 @@
+package javaCore.chapter02Condition.exercice;
+
+public class CoinChangerV2 {
+    public static void main(String[] args) {
+
+        final int BILLET_50 = 50;
+        final int BILLET_20 = 20;
+        final int BILLET_10 = 10;
+        final int BILLET_5  =  5;
+        final int BILLET_2  =  2;
+        final int BILLET_1  =  1;
+
+        // total de la facture à payer
+        int totalBill  = 66;
+        // montant que le client paye
+        int amountPaid = 72;
+        // montant restant à payer
+        int cashLeft   =  0;
+        // nombre de billet ou piece à rendre
+        int billChange =  0;
+
+        // ici la condition sert quand le montant à payer est inférieur à la facture
+        // on imagine ici celui qui paye, il remet la meme somme pour payer
+        if (amountPaid < totalBill) {
+            totalBill =  totalBill - amountPaid;
+            cashLeft  = amountPaid - totalBill;
+
+        } else {
+            cashLeft = amountPaid - totalBill;
+        }
+
+        if(cashLeft >= BILLET_50) {
+            billChange  = cashLeft / BILLET_50;
+            cashLeft    = cashLeft % BILLET_50;
+            System.out.println("j'ai rendu " + billChange + " billet de 50e et il reste " + cashLeft + " euros à rendre.");
+        }
+
+        if (cashLeft >= BILLET_20) {
+            billChange  = cashLeft / BILLET_20;
+            cashLeft    = cashLeft % BILLET_20;
+            System.out.println("j'ai rendu " + billChange + " billet de 20e et il reste " + cashLeft + " euros à rendre.");
+        }
+
+        if (cashLeft >= BILLET_10) {
+            billChange  = cashLeft / BILLET_10;
+            cashLeft    = cashLeft % BILLET_10;
+            System.out.println("j'ai rendu " + billChange + " billet de 10e et il reste " + cashLeft + " euros à rendre.");
+        }
+
+        if (cashLeft >= BILLET_5) {
+            billChange  = cashLeft / BILLET_5;
+            cashLeft    = cashLeft % BILLET_5;
+            System.out.println("j'ai rendu " + billChange + " billet de 5e et il reste " + cashLeft + " euros à rendre.");
+        }
+
+        if (cashLeft >= BILLET_2) {
+            billChange  = cashLeft / BILLET_2;
+            cashLeft    = cashLeft % BILLET_2;
+            System.out.println("J'ai rendu " + billChange + " piece de 2 euros et il reste " + cashLeft + " euros à rendre.");
+        }
+
+        if (cashLeft >= BILLET_1) {
+            billChange  = cashLeft / BILLET_1;
+            cashLeft    = cashLeft % BILLET_1;
+            System.out.println("J'ai rendu " + billChange + "piece de 1 euros et il reste " + cashLeft+ " euros à rendre.");
+        }
+
+
+    }
+    }
+}
