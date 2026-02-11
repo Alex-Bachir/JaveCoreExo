e59e44016130e3ab20ad51628ed44a571e35143a
Alex.Bachir <alex.bachir@laplateforme.io>
2026-02-11 11:54:16 +0100

    mise à jour de coinChangerV2

--- src/javaCore/chapter02Condition/exercice/CoinChangerV2.java
+++ src/javaCore/chapter02Condition/exercice/CoinChangerV2.java
@@ -4,27 +4,26 @@ public class CoinChangerV2 {
     public static void main(String[] args) {
 
         // total de la facture à payer
-        int totalBill  = 66;
+        int totalBill  = 100;
         // montant que le client paye
-        int amountPaid = 72;
+        int amountPaid = 250;
         // montant restant à rendre.
         int amountDue = amountPaid - totalBill;
-        // nombre de billet ou piece à rendre
-        int billChange =  0;
+
 
         final int BILLET_50_VALUE = 50;
         final int BILLET_20_VALUE = 20;
         final int BILLET_10_VALUE = 10;
         final int BILLET_5_VALUE  =  5;
         final int BILLET_2_VALUE  =  2;
-        final int BILLET_1_VALUE =  1;
+        final int BILLET_1_VALUE  =  1;
 
-        int billet50StockAvailable = 8;
-        int billet20StockAvailable = 7;
-        int billet10StockAvailable = 6;
-        int billet5StockAvailable = 5;
-        int billet2StockAvailable = 9;
-        int billet1StockAvailable = 8;
+        int billet50StockAvailable = 2;
+        int billet20StockAvailable = 4;
+        int billet10StockAvailable = 5;
+        int billet5StockAvailable  = 5;
+        int billet2StockAvailable  = 15;
+        int billet1StockAvailable  = 20;
 
         int cashLeft = ((BILLET_50_VALUE  *  billet50StockAvailable) +
                         (BILLET_20_VALUE  *  billet20StockAvailable) +
@@ -38,41 +37,85 @@ public class CoinChangerV2 {
         } else if (amountDue == 0) {
                 System.out.println("La facture à était payé dans sa totalité.");
         } else {
-            if (amountDue >= BILLET_50_VALUE) {
-                billChange  = amountDue / BILLET_50_VALUE;
-                amountDue    = amountDue % BILLET_50_VALUE;
-                System.out.println("j'ai rendu " + billChange + " billet de 50e et il reste " + amountDue + " euros à rendre.");
+            if (amountDue >= BILLET_50_VALUE && billet50StockAvailable > 0) {
+                // Nombre de billet de 50 requis
+                int requiredBillOf50  = amountDue / BILLET_50_VALUE;
+                // ici on récupère ce que je peux réellement donner
+                int billetToGive50    = billet50StockAvailable;
+                // Si le nombre de billet à rendre est supérieur à ce que je peux donner réellement.
+                // Je fais en sorte de donner tout mon stock puis on passe au billet inférieur.
+                if (requiredBillOf50  > billetToGive50) {
+                    billetToGive50 = billet50StockAvailable;
+                    amountDue -= billetToGive50 * BILLET_50_VALUE;
+                    System.out.println("J'ai rendu " + billetToGive50 + " billet de 50€ et il reste " + amountDue + " euros à rendre.");
+                    // ici dans le cas où le stock de billet est supérieur à ce qu'il faut donner c'est le cas classique.
+                } else {
+                    amountDue = amountDue % BILLET_50_VALUE;
+                    System.out.println("j'ai rendu " + requiredBillOf50 + " billet de 50e et il reste " + amountDue + " euros à rendre.");
+                }
+            }
+            if (amountDue >= BILLET_20_VALUE && billet20StockAvailable > 0) {
+                int requiredBillOf20 = amountDue / BILLET_20_VALUE;
+                int billetToGive20 = billet20StockAvailable;
+                if (requiredBillOf20 > billetToGive20) {
+                    billetToGive20 = billet20StockAvailable;
+                    amountDue -= billetToGive20 * BILLET_20_VALUE;
+                    System.out.println("j'ai rendu " + billetToGive20 + " billet de 20e et il reste " + amountDue + " euros à rendre.");
+                    } else {
+                    amountDue = amountDue % BILLET_20_VALUE;
+                    System.out.println("j'ai rendu " + requiredBillOf20 + " billet de 20e et il reste " + amountDue + " euros à rendre.");
+                    }
+            }
+            if (amountDue >= BILLET_10_VALUE && billet10StockAvailable > 0) {
+                int requiredBillOf10 = amountDue / BILLET_10_VALUE;
+                int billetToGive10 = billet10StockAvailable;
+                if (requiredBillOf10 > billetToGive10) {
+                    billetToGive10 = billet10StockAvailable;
+                    amountDue -= billetToGive10 * BILLET_10_VALUE;
+                    System.out.println("j'ai rendu " + billetToGive10 + " billet de 10e et il reste " + amountDue + " euros à rendre.");
+                } else {
+                    amountDue = amountDue % BILLET_10_VALUE;
+                    System.out.println("j'ai rendu " + requiredBillOf10 + " billet de 10e et il reste " + amountDue + " euros à rendre.");
+                }
+            }
+            if (amountDue >= BILLET_5_VALUE && billet5StockAvailable > 0) {
+                int requiredBillOf5 = amountDue / BILLET_5_VALUE;
+                int billetToGive5 = billet5StockAvailable;
+                if (requiredBillOf5 > billetToGive5) {
+                    billetToGive5 = billet5StockAvailable;
+                    amountDue -= billetToGive5 * BILLET_5_VALUE;
+                    System.out.println("j'ai rendu " + billetToGive5 + " billet de 5e et il reste " + amountDue + " euros à rendre.");
+                } else {
+                    amountDue = amountDue % BILLET_5_VALUE;
+                    System.out.println("j'ai rendu " + requiredBillOf5 + " billet de 5e et il reste " + amountDue + " euros à rendre.");
+                }
+            }
+            if (amountDue >= BILLET_2_VALUE && billet2StockAvailable > 0) {
+                int requiredBillOf2 = amountDue / BILLET_2_VALUE;
+                int billetToGive2 = billet2StockAvailable;
+                if (requiredBillOf2 > billetToGive2) {
+                    billetToGive2 = billet2StockAvailable;
+                    amountDue -= billetToGive2 * BILLET_2_VALUE;
+                    System.out.println("j'ai rendu " + billetToGive2 + " pièce de 2e et il reste " + amountDue + " euros à rendre.");
+                } else {
+                    amountDue = amountDue % BILLET_2_VALUE;
+                    System.out.println("j'ai rendu " + requiredBillOf2 + " pièce de 2e et il reste " + amountDue + " euros à rendre.");
+                }
+            }
+            if (amountDue >= BILLET_1_VALUE && billet1StockAvailable > 0) {
+                int requiredBillOf1 = amountDue / BILLET_1_VALUE;
+                int billetToGive1 = billet1StockAvailable;
+                if (requiredBillOf1 > billetToGive1) {
+                    billetToGive1 = billet1StockAvailable;
+                    amountDue -= billetToGive1 * BILLET_1_VALUE;
+                    System.out.println("j'ai rendu " + billetToGive1 + " pièce de 1e et il reste " + amountDue + " euros à rendre.");
+                } else {
+                    amountDue = amountDue % BILLET_1_VALUE;
+                    System.out.println("j'ai rendu " + requiredBillOf1 + " pièce de 1e et il reste " + amountDue + " euros à rendre.");
+                }
             }
 
-            if (amountDue >= BILLET_20_VALUE) {
-                billChange  = amountDue / BILLET_20_VALUE;
-                amountDue    = amountDue % BILLET_20_VALUE;
-                System.out.println("j'ai rendu " + billChange + " billet de 20e et il reste " + amountDue + " euros à rendre.");
-            }
 
-            if (amountDue >= BILLET_10_VALUE) {
-                billChange  = amountDue / BILLET_10_VALUE;
-                amountDue    = amountDue % BILLET_10_VALUE;
-                System.out.println("j'ai rendu " + billChange + " billet de 10e et il reste " + amountDue + " euros à rendre.");
-            }
-
-            if (amountDue >= BILLET_5_VALUE) {
-                billChange  = amountDue / BILLET_5_VALUE;
-                amountDue    = amountDue % BILLET_5_VALUE;
-                System.out.println("j'ai rendu " + billChange + " billet de 5e et il reste " + amountDue + " euros à rendre.");
-            }
-
-            if (amountDue >= BILLET_2_VALUE) {
-                billChange  = amountDue / BILLET_2_VALUE;
-                amountDue    = amountDue % BILLET_2_VALUE;
-                System.out.println("J'ai rendu " + billChange + " piece de 2 euros et il reste " + amountDue + " euros à rendre.");
-            }
-
-            if (amountDue >= BILLET_1_VALUE) {
-                billChange  = amountDue / BILLET_1_VALUE;
-                amountDue    = amountDue % BILLET_1_VALUE;
-                System.out.println("J'ai rendu " + billChange + "piece de 1 euros et il reste " + amountDue+ " euros à rendre.");
-            }
         }
     }
 }
