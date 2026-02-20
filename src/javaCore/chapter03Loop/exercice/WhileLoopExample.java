0edebe3f6ad80593e928185b1676fed12d427cf2
Alex.Bachir <alex.bachir@laplateforme.io>
2026-02-20 15:32:17 +0100

    LoopExample terminé

--- src/javaCore/chapter03Loop/exercice/WhileLoopExample.java
+++ src/javaCore/chapter03Loop/exercice/WhileLoopExample.java
@@ -3,5 +3,11 @@ package javaCore.chapter03Loop.exercice;
 public class WhileLoopExample {
     public static void main (String[] args) {
 
+        int loopCounter = 1;
+
+        while (loopCounter <= 1000) {
+            System.out.println(loopCounter + " Hello World");
+            loopCounter++;
+        }
     }
 }
