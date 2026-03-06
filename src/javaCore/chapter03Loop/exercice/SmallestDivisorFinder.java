05becc7ad958e0943e17e8260be79e323713ad32
Alex.Bachir <alex.bachir@laplateforme.io>
2026-03-06 09:15:36 +0100

    smalestDivisorFinder

--- src/javaCore/chapter03Loop/exercice/SmallestDivisorFinder.java
+++ src/javaCore/chapter03Loop/exercice/SmallestDivisorFinder.java
@@ -3,13 +3,17 @@ package javaCore.chapter03Loop.exercice;
 public class SmallestDivisorFinder {
     public static void main (String[] args) {
 
-        int number = 212;
-        int divisor = 3;
+        int number = 43;
+        int divisor = 2;
 
         while (number % divisor != 0) {
-            int result = number % divisor;
-            System.out.println(result);
             divisor ++;
+        }
+
+        if (number % divisor == 0 && number == divisor) {
+            System.out.println(number + " ce chiffre peut ce diviser que par lui même.");
+        } else {
+            System.out.println(divisor + " ce chiffre est le plus petit diviseur pour " + number);
         }
     }
 }
