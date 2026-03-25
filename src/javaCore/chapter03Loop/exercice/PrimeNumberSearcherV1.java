7197494b9d603df4a05374edd83b5d8dd9fff35a
Alex.Bachir <alex.bachir@laplateforme.io>
2026-03-25 13:29:01 +0100

    mmodification PrimeNumberSearcher

--- src/javaCore/chapter03Loop/exercice/PrimeNumberSearcherV1.java
+++ src/javaCore/chapter03Loop/exercice/PrimeNumberSearcherV1.java
@@ -8,19 +8,19 @@ public class PrimeNumberSearcherV1 {
 //
 
         // premiere étape on itère jusqu'à 100.
-        for (int i = 2; i <= 100; i++) {
+        for (int potentialFirstNumber = 2; potentialFirstNumber <= 100; potentialFirstNumber++) {
             // on crée un compteur initialisé à 0.
             int count = 0;
             // la boucle imbriquée récupére le chiffre et fait une verification jusqu'à qu'il trouve un diviseur,
             // on incrémente count à chaque fois qu'on trouve un diviseur avec lequel il n'y'a pas de reste.
-            for (int j = 2; j < i; j++) {
-                if (i % j == 0) {
+            for (int potentialDivisor = 2; potentialDivisor < potentialFirstNumber; potentialDivisor++) {
+                if (potentialFirstNumber % potentialDivisor == 0) {
                     count++;
                 }
             }
             // On conclut, si on n'a pas trouvé de diviseur alors c'est un nombre premier
             if (count == 0) {
-                System.out.print(i + " ");
+                System.out.print(potentialFirstNumber + " ");
             }
         }
     }
