21d1c89b23c2f8c08f272d08c9ecc4e9388c7c92
Alex.Bachir <alex.bachir@laplateforme.io>
2026-04-13 11:11:01 +0200

    FibonacciCalculator

---
+++ src/javaCore/chapter02Condition/exercice/FibonacciCalculator.java
@@ -0,0 +1,31 @@
+package javaCore.chapter02Condition.exercice;
+
+public class FibonacciCalculator {
+    public static void main(String[] args) {
+
+        long firstInitialTerm   = 0;
+        long secondInitialTerm  = 1;
+        int countPair   = 0;
+        int countImpair = 0;
+
+
+        for (int i = 0; i <= 50; i++) {
+            long temporaryTerm = firstInitialTerm + secondInitialTerm;
+            firstInitialTerm = secondInitialTerm;
+            secondInitialTerm = temporaryTerm;
+            if (temporaryTerm % 2 == 0) {
+                System.out.println(temporaryTerm + "il est pair");
+                countPair++;
+            } else {
+                System.out.println(temporaryTerm + "il est impair");
+                countImpair++;
+            }
+        }
+
+        System.out.println("Il y'a au total " + countPair + " chiffre pair." +
+                " Il y'a au total " + countImpair + " chiffre impair.");
+
+
+
+    }
+}
