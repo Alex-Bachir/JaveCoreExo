4ea4365d6a3786d55ee999879309e2a8439f8ab2
Alex.Bachir <alex.bachir@laplateforme.io>
2026-04-14 09:45:33 +0200

    MultiplicationTables finish

--- src/javaCore/chapter03Loop/exercice/MultiplicationTables.java
+++ src/javaCore/chapter03Loop/exercice/MultiplicationTables.java
@@ -3,5 +3,14 @@ package javaCore.chapter03Loop.exercice;
 public class MultiplicationTables {
     public static void main(String[] args) {
 
+        for(int table = 1; table <= 10; table++){
+            System.out.println("Table de " + table + " :");
+            System.out.println();
+            for(int number = 1; number <= 10; number++){
+                int result = table * number;
+                System.out.println(table + " * " + number + " = " + result);
+            }
+            System.out.println();
+        }
     }
 }
