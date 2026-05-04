f2d2c874abf69a275c7078518bd44daa0b67015d
Alex.Bachir <alex.bachir@laplateforme.io>
2026-05-04 13:46:05 +0200

    FunctionOverload finish

--- src/javaCore/Chapter04Function/exercise/FunctionOverload.java
+++ src/javaCore/Chapter04Function/exercise/FunctionOverload.java
@@ -1,4 +1,20 @@
 package javaCore.Chapter04Function.exercise;
 
 public class FunctionOverload {
+
+    public static void main(String[] args) {
+
+        int result1 = sum(2, 8, 5);
+        System.out.println(result1);
+
+
+
+    }
+
+    public static int sum(int number1, int number2, int number3) {
+        return number1 + number2 + number3;
+    }
+
+
+
 }
