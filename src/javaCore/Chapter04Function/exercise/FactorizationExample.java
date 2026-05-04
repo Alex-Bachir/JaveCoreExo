3b0c6fc3c59ddb08c9bc70fc0a8953bcc5549011
Alex.Bachir <alex.bachir@laplateforme.io>
2026-05-04 13:39:53 +0200

    finish FactorizationExample

--- src/javaCore/Chapter04Function/exercise/FactorizationExample.java
+++ src/javaCore/Chapter04Function/exercise/FactorizationExample.java
@@ -3,5 +3,23 @@ package javaCore.Chapter04Function.exercise;
 public class FactorizationExample {
     public static void main(String[] args) {
 
+        affichage("1");
+        affichage("2");
+        affichage("3");
+        affichage("4");
+        affichage("5");
+        affichage("6");
+        affichage("7");
+        affichage("8");
+        affichage("9");
+        affichage("10");
+
+
     }
+
+    public static void affichage(String message) {
+        // out et err on peut constater que le changement c'est propager dans la methode
+        System.out.println(message);
+    }
+
 }
