d0fe425c822b01ff98e4bdceebfb6feaa9c2f4be
Alex.Bachir <alex.bachir@laplateforme.io>
2026-05-13 11:41:21 +0200

    add commentary PrimeNumberSearcherV3

--- src/javaCore/Chapter04Function/exercise/PrimeNumberSearcherV3.java
+++ src/javaCore/Chapter04Function/exercise/PrimeNumberSearcherV3.java
@@ -4,23 +4,20 @@ public class PrimeNumberSearcherV3 {
 
     public static void main(String[] args) {
 
-
+        // la déclaration de itérationCount va servir de compteur pour les nombre premier
         int iterationCount = 1;
-
+        // la boucle va itérer jusqu'a 50
         for (int potentialFirstNumber = 2; potentialFirstNumber <= 50; potentialFirstNumber++) {
-
-            if (isPrimeNumber( potentialFirstNumber)) {
-
-                    System.out.println("Le chiffre " + potentialFirstNumber + " est le numero " + iterationCount++ + " des nombres premier.");
-            }
+            // on appel la methode isPrimeNumber quand c'est true alors on affiche le nombre
+            if (isPrimeNumber(potentialFirstNumber))
+                // la premiere itération n'est pas adapter c'est pas grave x)
+                System.out.println(potentialFirstNumber + " est le " + iterationCount++ + "ème nombre premier.");            }
         }
 
 
-
-    }
-
+    // la méthode et un boolean
     public static boolean isPrimeNumber(int potentialFirstNumber) {
-
+        // retourne false dès qu'un diviseur est trouvé, true si aucun diviseur trouvé après la boucle
         for (int potentialDivisor = 2; potentialDivisor < potentialFirstNumber; potentialDivisor++) {
 
             if (potentialFirstNumber % potentialDivisor == 0) {
@@ -30,8 +27,5 @@ public class PrimeNumberSearcherV3 {
         }
 
         return true;
-
-
-
     }
 }
